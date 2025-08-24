6f203c0a5adf0fb85d682e3e85c876fe2b513ad6
NicholasNPham <phamn1000@gmail.com>
2025-08-24 14:10:13 -0400

    finished Lab 2

---
+++ M1/Lab1_B.py
@@ -0,0 +1,6 @@
+# Sales Tax Calculator
+
+price = float(input("Enter the price of the item: "))
+taxPercentage = float(input("Enter the sales tax percentage: ")) / 100
+totalPrice = price + (taxPercentage * price)
+print(f"Your total is {totalPrice:.2f}")
