0a515a288c7ed5d56409f4bf94c2a6b0de96df96
NicholasNPham <phamn1000@gmail.com>
2025-08-24 00:15:39 -0400

    did not know that the round function is rounded to the nearest even integer

--- one.py
+++ one.py
@@ -2,3 +2,10 @@
 name = "Nicholas Pham"
 stringName = f'My name is {name}'
 print(stringName)
+
+# calling round (round function is also known as banker's round)
+a = 2.5
+b = 3.5
+print(round(a))
+print(round(b))
+
