b979ac925c3c00a29b93751c8c82a576b5c5b03a
NicholasNPham <phamn1000@gmail.com>
2025-08-24 00:46:22 -0400

    added notes and  in py file and learning what modulo operator is actually is and a basic program to determine final grade in course

--- one.py
+++ one.py
@@ -13,3 +13,21 @@ print(round(b))
 intOne = 10
 intTwo = 3
 print(intOne % intTwo) # the remainder of this equation is 1 printing 1
+
+# Using int() and float()
+first = float(input("First number: "))
+second = float(input("Second number: "))
+print(first + second)
+
+# Practice Problem: Write a program to calculate the final grade of the course
+test = float(input("Test Score Average: "))
+quiz = float(input("Quiz Score Average: "))
+assignment = float(input("Assignment Score Average: "))
+project = float(input("Project Score Average: "))
+
+finalGrade = test * .4 + quiz * .3 + assignment * .1 + project * .2
+print(finalGrade)
+
+"""
+this is very fundamental comparing to the project posted on this github especially comparing to the notionAIOptimizer project, good to learn fundamentals on an institutional level though.
+"""
