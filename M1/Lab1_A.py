170f8791d179f45ad592ebba3fe68b2457bb8b93
NicholasNPham <phamn1000@gmail.com>
2025-08-24 13:53:44 -0400

    Finished Lab 1

---
+++ M1/Lab1_A.py
@@ -0,0 +1,5 @@
+# Function: Celsius to Fahrenheit
+
+celsius = float(input("Enter the temperature in Celsius: "))
+farenheit = (9 / 5) * celsius + 32
+print(f"That is {farenheit:.1f} degrees fahrenheit.")
