6fba3d5cb391a9d790caed1fda2017564b73afa1
NicholasNPham <phamn1000@gmail.com>
2025-08-25 12:15:41 -0400

    fixed typos

--- M1/Lab1_A.py
+++ M1/Lab1_A.py
@@ -2,4 +2,4 @@
 
 celsius = float(input("Enter the temperature in Celsius: "))
 farenheit = (9 / 5) * celsius + 32
-print(f"That is {farenheit:.1f} degrees fahrenheit.")
+print(f"That is {farenheit:.1f} degrees Fahrenheit!")
--- M1/Lab1_B.py
+++ M1/Lab1_B.py
@@ -3,4 +3,4 @@
 price = float(input("Enter the price of the item: "))
 taxPercentage = float(input("Enter the sales tax percentage: ")) / 100
 totalPrice = price + (taxPercentage * price)
-print(f"Your total is {totalPrice:.2f}")
+print(f"Your total is ${totalPrice:.2f}")
