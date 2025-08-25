b4a362871179846363f969c54cb0b93d35336ebf
NicholasNPham <phamn1000@gmail.com>
2025-08-25 12:06:31 -0400

    figured out optimial way, had if else statement to produce an positive int, also could do abs() but for the purpose of the class learning fund 1

--- M1/Lab1_C.py
+++ M1/Lab1_C.py
@@ -1,33 +1,19 @@
 # Calculate how many days are in between two dates
 
-yearOne = 1999 # int(input("Enter the year for date 1: "))
-monthOne = 12 # int(input("Enter the month for date 1: "))
-dayOne = 30 # int(input("Enter the day for date 1: "))
-yearTwo = 2000 # int(input("Enter the year for date 2: "))
-monthTwo = 1 # int(input("Enter the month for date 2: "))
-dayTwo = 1  # int(input("Enter the day for date 2: "))
+yearOne = int(input("Enter the year for date 1: "))
+monthOne = int(input("Enter the month for date 1: "))
+dayOne = int(input("Enter the day for date 1: "))
+yearTwo = int(input("Enter the year for date 2: "))
+monthTwo = int(input("Enter the month for date 2: "))
+dayTwo = int(input("Enter the day for date 2: "))
 
-if yearOne > yearTwo:
-    yearInt = yearOne - yearTwo
+daysInFirstDate = (yearOne * 12) * 30 + monthOne * 30 + dayOne
+daysInSecondDate = (yearTwo * 12) * 30 + monthTwo * 30 + dayTwo
+
+if daysInFirstDate > daysInSecondDate:
+    totalDays = daysInFirstDate - daysInSecondDate
 else:
-    yearInt = yearTwo - yearOne
-
-if monthOne > monthTwo:
-    monthInt = monthOne - monthTwo
-else:
-    monthInt = monthTwo - monthOne
-
-if dayOne > dayTwo:
-    dayInt = dayOne - dayTwo
-else:
-    dayInt = dayTwo - dayOne
-
-print(yearInt, monthInt, dayInt)
-
-daysInYear = (yearInt * 12) * 30
-daysInMonth = monthInt * 30
-
-totalDays = daysInYear + daysInMonth + dayInt
+    totalDays = daysInSecondDate - daysInFirstDate
 
 output = f"The difference between {monthOne}/{dayOne}/{yearOne} and {monthTwo}/{dayTwo}/{yearTwo} is {totalDays} days!"
 print(output)
