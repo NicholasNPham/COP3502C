3835000f255a624e8a0ff44944d1c90690e51c7f
NicholasNPham <phamn1000@gmail.com>
2025-08-27 13:05:13 -0400

    debugged, fixed to match correct output, was not updated tied games, with my spare free time im going to optimize the hell out of this code, this code looks terrible to me and I know i can do better

--- M1/P1.py
+++ M1/P1.py
@@ -10,7 +10,7 @@ gameProgress = True
 while gameProgress:
     # New Game Loop
     gameNumber += 1
-    print(f'Start Game #{gameNumber}')
+    print(f'START GAME #{gameNumber}')
     card = rng.next_int(13) + 1 #[0-12]
     # Resetting Hand Count
     hand = card
@@ -29,32 +29,32 @@ while gameProgress:
     else:
         hand = 10
         card = "KING"
-    print(f'\nYour card is {card}!')
+    print(f'\nYour card is a {card}!')
     print(f'Your hand is: {hand}')
     while True:
-        choice = int(input("\n1. Get Another Card \n2. Hold Hand \n3. Print statistics \n4. Exit \n\nChoose your choice: "))
+        choice = int(input("\n1. Get another card\n2. Hold hand\n3. Print statistics\n4. Exit\n\nChoose an option: "))
         if choice == 1:
             card = rng.next_int(13) + 1
             if card == 1:
                 hand += card
-                print('Your card is ACE!')
-                print(f'Your hand is {hand}')
+                print('Your card is a ACE!')
+                print(f'Your hand is: {hand}')
             elif 2 <= card <= 10:
                 hand += card
-                print(f'Your card is {card}!')
-                print(f'Your hand is {hand}')
+                print(f'Your card is a {card}!')
+                print(f'Your hand is: {hand}')
             elif card == 11:
                 hand += 10
-                print(f'Your card is JACK!')
-                print(f'Your hand is {hand}')
+                print(f'Your card is a JACK!')
+                print(f'Your hand is: {hand}')
             elif card == 12:
                 hand += 10
-                print(f'Your card is QUEEN!')
-                print(f'Your hand is {hand}')
+                print(f'Your card is a QUEEN!')
+                print(f'Your hand is: {hand}')
             elif card == 13:
                 hand += 10
-                print(f'Your card is KING!')
-                print(f'Your hand is {hand}')
+                print(f'Your card is a KING!')
+                print(f'Your hand is: {hand}')
 
             if hand == 21:
                 playerWin += 1
@@ -69,34 +69,34 @@ while gameProgress:
             # Dealer's Draw
             dealerHand = rng.next_int(11) + 16
             print(f"\nDealer's hand: {dealerHand}")
-            print(f"Your hand is {hand}")
+            print(f"Your hand is: {hand}")
             if dealerHand == 21:
                 dealerWin += 1
-                print("\nDealer Wins!\n")
+                print("\nDealer wins!\n")
                 break
             elif dealerHand > 21:
                 playerWin += 1
-                print("\nYou Win!\n")
+                print("\nYou win!\n")
                 break
             elif hand == dealerHand:
                 print("\nIt's a tie! No one wins!\n")
-                gameProgress += 1
+                gameTied += 1
                 break
             elif hand > dealerHand:
                 playerWin += 1
-                print("\nYou Win!\n")
+                print("\nYou win!\n")
                 break
-            elif dealerHand < hand:
+            else:
                 dealerWin += 1
-                print("\nDealer Wins!\n")
+                print("\nDealer wins!\n")
                 break
 
         elif choice == 3:
             print(f'Number of Player wins: {playerWin}')
             print(f'Number of Dealer wins: {dealerWin}')
             print(f'Number of tie games: {gameTied}')
-            print(f'total # of games is: {gameNumber - 1}')
-            print(f'Percentage of Player wins: {playerWin/(gameNumber - 1):.2f}%')
+            print(f'Total # of games played is: {gameNumber - 1}')
+            print(f'Percentage of Player wins: {(playerWin/(gameNumber - 1))* 100:.1f}%')
 
         elif choice == 4:
             gameProgress = False
