5d780d4efbce461a5c4bb5e1cb5cfb642f2aa920
NicholasNPham <phamn1000@gmail.com>
2025-09-10 00:10:16 -0400

    solved palindromic problem, very nice

--- M3/lecture3.py
+++ M3/lecture3.py
@@ -1,1 +1,23 @@
+num = str(input("enter a number: "))
+list = []
+reversedList = []
+
+for i in num:
+    list.append(str(i))
+
+for j in range(l-1, -1, -1):
+    reversedList.append(list[j])
+
+l = len(list) # length
+counter = 0
+
+while counter < l:
+    if reversedList[counter] == list[counter]:
+        counter += 1
+        if counter == l:
+            print(f"{num} is a Palindrome")
+            break
+    else:
+        print(f"{num} is not a Palindromic Number")
+        break
 
