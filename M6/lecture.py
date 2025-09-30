0e4233aa965dd609e9bbc3088aa14e06a56cec3a
NicholasNPham <phamn1000@gmail.com>
2025-09-29 23:10:01 -0400

    added lecture notes

---
+++ M6/lecture.py
@@ -0,0 +1,30 @@
+"""
+Number Systems:
+
+Decimal -> (10)^(0-9) digits (We Use base 10 Systems)
+- Example: 217 = (2/10^2)(1/10^1)(7/10^0) = 2 * 100 + 1 * 10 + 7 * 1
+
+Binary  -> (2)^(0,1) digit
+- Example: 101 = (1/2^2)(0/2^1)(1/2^0) = 1 * 4 + 0 * 2 + 1 * 1 = 5 <- this 5 represents the decimal representation of binary 101
+
+Decimal -> Binary
+- Example: Look at Notes for Example
+
+Octal -> (8)^(0-7) digits
+- Example: 207 = (2/8^2)(0/8^1)(7/8^0) = 2 * 64 + 0 * 8 + 7 * 1 =  135 <- this 135 represents the decimal representation of octal 207
+
+Hexadecimal -> (16)^(0-15) digits
+(0-15) = (1, 2, 3, 4, 5, 6, 7, 8, 9, (10=A), (11=B), (12=C), (13=D), (14=E), (15=F))
+Example: 5A = = (5/16^1)(A/16^0) = 5 * 16 + 10 * 1 = 81 <- this 81 represents the decimal representation of hexadecimal 5A
+
+Number Systems:
+- In Mathmatics, a base is the number of different digits or combinations of digits and letters that a system of counting uses to represents numbers
+
+Binary Representation:
+- Computers use binary for all data. Numbers are stored using binary numbering
+- Characters are  stored as numbers which represent their symbols
+
+Practice Question: Convert binary number 01001001 to decimal number.
+
+Stopped at 12:47
+"""
