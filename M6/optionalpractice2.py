1e9a5a6a33c51a22447b7937c7a9100fe2b93dd9
NicholasNPham <phamn1000@gmail.com>
2025-10-04 22:41:43 -0400

    solved problem 2

--- M6/optionalpractice2.py
+++ M6/optionalpractice2.py
@@ -9,7 +9,20 @@ def parse_student(string):
 
     return studentDict
 
-print(parse_student("12345678Will Albright0116"))
-print(parse_student("87654321Leah Zabad0221"))
-print(parse_student("00000001Adrian Moreno0225"))
-print(parse_student("00000007Random Student0427"))
+# print(parse_student("12345678Will Albright0116"))
+# print(parse_student("87654321Leah Zabad0221"))
+# print(parse_student("00000001Adrian Moreno0225"))
+# print(parse_student("00000007Random Student0427"))
+
+def count_items(mylist):
+    dictionary = {}
+
+    for item in mylist:
+        dictionary[item] = dictionary.get(item, 0) + 1
+
+    return dictionary
+
+# print(count_items([1, 1, 1, 2, 2, 2, 2, 2, 3, 3, 3, 3, 3, 3, 3]))
+# print(count_items(["a", "b", "c", "a", "aa", "c", "hello"]))
+# print(count_items([0, 5, 4, 6, 5, 5, 9, 5, 4, 4, 8, 8, 9, 4, 9]))
+# print(count_items("hello world"))
