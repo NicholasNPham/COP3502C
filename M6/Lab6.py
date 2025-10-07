3a8ee214ab816543086d5d71f5fd41cb94d3975c
NicholasNPham <phamn1000@gmail.com>
2025-10-07 11:03:14 -0400

    started lab6

---
+++ M6/Lab6.py
@@ -0,0 +1,19 @@
+def initialize_board(num_rows, num_cols):
+    pass
+
+def print_board(board):
+    pass
+
+def insert_chip(board,col,chip_type):
+    pass
+
+def check_if_winner(board,col,row,chip_type):
+    pass
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
+
+
