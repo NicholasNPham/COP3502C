a7174db88f8939029383d55e49b4664065c96f01
NicholasNPham <phamn1000@gmail.com>
2025-10-07 11:03:59 -0400

    finished created board function, working on print board function, might be backwards

--- M6/Lab6.py
+++ M6/Lab6.py
@@ -1,8 +1,22 @@
 def initialize_board(num_rows, num_cols):
-    pass
+
+    board = []
+
+    for rows in range(num_rows):
+        r = []
+        for cols in range(num_cols):
+            r.append("_")
+        board.append(r)
+
+    return board
 
 def print_board(board):
-    pass
+
+    board = board
+    for row in board:
+        for space in row:
+            print(space, end=" ")
+        print()
 
 def insert_chip(board,col,chip_type):
     pass
@@ -11,7 +25,11 @@ def check_if_winner(board,col,row,chip_type):
     pass
 
 def main():
-    pass
+
+    height = int(input("What would you like the height of the board to be? "))
+    length = int(input("What would you like the length of the board to be? "))
+
+    print_board(initialize_board(height,length))
 
 if __name__ == '__main__':
     main()
