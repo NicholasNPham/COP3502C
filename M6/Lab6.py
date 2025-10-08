84cfe1bf029d40cb480f8a7d0f9797ddcc65c9d2
NicholasNPham <phamn1000@gmail.com>
2025-10-08 02:30:57 -0400

    working insert chip function bug is allowing user to use full column as choice

--- M6/Lab6.py
+++ M6/Lab6.py
@@ -19,16 +19,17 @@ def print_board(board):
         print()
 
 def insert_chip(board,col,chip_type):
-    row = 0
     col = col
     chip_type = chip_type
     board = board
 
-    if board[row][col] == "x" or board[row][col] == "o":
-        row += 1
-        board[row][col] = chip_type
-    else:
-        board[row][col] = chip_type
+    for row in range(len(board)):
+        if board[row][col] == "x" or board[row][col] == "o":
+            row += 1
+            continue
+        else:
+            board[row][col] = chip_type
+            break
 
 def check_if_winner(board,col,row,chip_type):
     pass
@@ -40,7 +41,6 @@ def main():
 
     print_board(initialize_board(height,length))
     board = initialize_board(height,length)
-    # print(board) # This is the print the list
 
     print("\nPlayer 1: x")
     print("Player 2: 0\n")
@@ -49,26 +49,27 @@ def main():
 
     while True:
 
-            if player1 % 2 == 0:
-                chip_type = "x"
-                col = int(input("Player 1: Which column would you like to choose? "))
-                if col <= len(board):
-                    insert_chip(board, col, chip_type)
-                    player1 += 1
-                    print_board(board)
-                else:
-                    print("Enter Valid Column")
-                    continue
+        if player1 % 2 == 0:
+            chip_type = "x"
+            col = int(input("Player 1: Which column would you like to choose? "))
+            if col <= len(board):
+                insert_chip(board, col, chip_type)
+                player1 += 1
+                print_board(board)
             else:
-                chip_type = "o"
-                col = int(input("Player 2: Which column would you like to choose? "))
-                if col <= len(board):
-                    insert_chip(board, col, chip_type)
-                    player1 += 1
-                    print_board(board)
-                else:
-                    print("Enter Valid Column")
-                    continue
+                print("Enter Valid Column")
+                continue
+        else:
+            chip_type = "o"
+            col = int(input("Player 2: Which column would you like to choose? "))
+            if col <= len(board):
+                insert_chip(board, col, chip_type)
+                player1 += 1
+                print_board(board)
+            else:
+                print("Enter Valid Column")
+                continue
+        # print(board) # This is the print the list
 
 if __name__ == '__main__':
     main()
