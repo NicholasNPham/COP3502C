8feea7f801efc18800057fc1c66ff39133edac42
NicholasNPham <phamn1000@gmail.com>
2025-10-08 16:40:37 -0400

    this was a hard lab

--- M6/Lab6.py
+++ M6/Lab6.py
@@ -5,7 +5,7 @@ def initialize_board(num_rows, num_cols):
     for rows in range(num_rows):
         r = []
         for cols in range(num_cols):
-            r.append("_")
+            r.append("-")
         board.append(r)
 
     return board
@@ -24,15 +24,56 @@ def insert_chip(board,col,chip_type):
     board = board
 
     for row in range(len(board)):
-        if board[row][col] == "x" or board[row][col] == "o":
-            row += 1
-            continue
-        else:
+        if board[row][col] == "-":
             board[row][col] = chip_type
-            break
+            return row
+    return None
 
 def check_if_winner(board,col,row,chip_type):
-    pass
+
+    # Vertical Check
+    count = 1
+
+    # up
+    r = row - 1
+    while r >= 0 and board[r][col] == chip_type:
+        count += 1
+        r -= 1
+    # down
+    r = row + 1
+    while r < len(board) and board[r][col] == chip_type:
+        count += 1
+        r += 1
+
+    if count >= 4:
+        return True
+
+    # Horizontal check
+    count = 1
+
+    # look left
+    c = col - 1
+    while c >= 0 and board[row][c] == chip_type:
+        count += 1
+        c -= 1
+
+    # look right
+    c = col + 1
+    while c < len(board[0]) and board[row][c] == chip_type:
+        count += 1
+        c += 1
+
+    if count >= 4:
+        return True
+
+    return False
+
+def check_draw(board):
+    for row in board:
+        for space in row:
+            if space == "-":
+                return False
+    return True
 
 def main():
 
@@ -43,7 +84,7 @@ def main():
     board = initialize_board(height,length)
 
     print("\nPlayer 1: x")
-    print("Player 2: 0\n")
+    print("Player 2: o\n")
 
     player1 = 0
 
@@ -58,13 +99,22 @@ def main():
                 print("Enter Valid Column")
                 continue
             else:
-                if board[-1][col] != "_":
-                        print("Column is Full, Choose Another")
-                        continue
+                row = insert_chip(board,col,chip_type[0])
+                if row is None:
+                    print("Column is full, Choose Another")
+                    continue
                 else:
-                    insert_chip(board, col, chip_type[0])
                     player1 += 1
                     print_board(board)
+
+                if check_if_winner(board,col,row,"x"):
+                    print("Player 1 won the game!")
+                    break
+
+                if check_draw(board):
+                    print("Draw. Nobody wins.")
+                    break
+
 
         # Player 2
         else:
@@ -75,17 +125,23 @@ def main():
                 print("Enter Valid Column")
                 continue
             else:
-                if board[-1][col] != "_":
-                    print("Column is Full, Choose Another")
+                row = insert_chip(board, col, "o")
+                if row is None:
+                    print("Column is full, Choose Another")
                     continue
                 else:
-                    insert_chip(board, col, chip_type[0])
                     player1 += 1
                     print_board(board)
+
+                if check_if_winner(board, col, row, chip_type[0]):
+                    print("Player 2 won the game!")
+                    break
+
+                if check_draw(board):
+                    print("Draw. Nobody wins.")
+                    break
 
         # print(board) # This is the print the list
 
 if __name__ == '__main__':
     main()
-
-
