92a63f4d6b0b900d3fca24aa504a8a496cb13481
NicholasNPham <phamn1000@gmail.com>
2025-10-08 02:04:54 -0400

    added alot, working x and o problem with insert chiptype I know why but cant think of how to check column

--- M6/Lab6.py
+++ M6/Lab6.py
@@ -13,13 +13,22 @@ def initialize_board(num_rows, num_cols):
 def print_board(board):
 
     board = board
-    for row in board:
+    for row in board[::-1]:
         for space in row:
             print(space, end=" ")
         print()
 
 def insert_chip(board,col,chip_type):
-    pass
+    row = 0
+    col = col
+    chip_type = chip_type
+    board = board
+
+    if board[row][col] == "x" or board[row][col] == "o":
+        row += 1
+        board[row][col] = chip_type
+    else:
+        board[row][col] = chip_type
 
 def check_if_winner(board,col,row,chip_type):
     pass
@@ -30,6 +39,36 @@ def main():
     length = int(input("What would you like the length of the board to be? "))
 
     print_board(initialize_board(height,length))
+    board = initialize_board(height,length)
+    # print(board) # This is the print the list
+
+    print("\nPlayer 1: x")
+    print("Player 2: 0\n")
+
+    player1 = 0
+
+    while True:
+
+            if player1 % 2 == 0:
+                chip_type = "x"
+                col = int(input("Player 1: Which column would you like to choose? "))
+                if col <= len(board):
+                    insert_chip(board, col, chip_type)
+                    player1 += 1
+                    print_board(board)
+                else:
+                    print("Enter Valid Column")
+                    continue
+            else:
+                chip_type = "o"
+                col = int(input("Player 2: Which column would you like to choose? "))
+                if col <= len(board):
+                    insert_chip(board, col, chip_type)
+                    player1 += 1
+                    print_board(board)
+                else:
+                    print("Enter Valid Column")
+                    continue
 
 if __name__ == '__main__':
     main()
