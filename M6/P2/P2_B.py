3b08e47d231d65a78daa534d6f9510b49a53dff0
NicholasNPham <phamn1000@gmail.com>
2025-10-09 16:32:03 -0400

    start of P2_B.py

---
+++ M6/P2/P2_B.py
@@ -0,0 +1,38 @@
+import console_gfx as console_gfx
+
+# console_gfx.display_image(console_gfx.test_rainbow)
+
+def display_menu():
+    print("\nRLE Menu\n"
+          "--------")
+    print("0. Exit")
+    print("1. Load File")
+    print("2. Load Test Image")
+    print("3. Read RLE String")
+    print("4. Read RLE Hex String")
+    print("5. Read Data Hex String")
+    print("6. Display Image")
+    print("7. Display RLE String")
+    print("8. Display Hex RLE Data")
+    print("9. Display Hex Flat Data\n")
+
+def main():
+    print("Welcome to the RLE image encoder!")
+    print("Displaying Spectrum Image:")
+    console_gfx.display_image(console_gfx.test_rainbow)
+    while True:
+        display_menu()
+        option = int(input("Select a Menu Option: "))
+        if option == 0:
+            break
+        elif option == 1:
+            filename = input("Enter name of file to load: ")
+            image_data = console_gfx.load_file(filename)
+        elif option == 2:
+            image_data = console_gfx.test_image
+            print("Test image data loaded.")
+        elif option == 6:
+            console_gfx.display_image(image_data)
+
+if __name__ == "__main__":
+    main()
