873e7e2254b8a3a690edcdedd5afef5423a89482
NicholasNPham <phamn1000@gmail.com>
2025-10-22 11:28:32 -0400

    smol recursive power problem

--- M9/recursionpractice.py
+++ M9/recursionpractice.py
@@ -131,3 +131,11 @@ def nested_sum(nest):
 print(nested_sum([1, [], 2, [3, 4], [5, [6, 7]]]))
 print(nested_sum([[1, 2], [3, [4, [5, 6]]], [7, [8, [9, [10]]]]]))
 print(nested_sum([1, [2, [3, [4], 5], [6, [7, [8], 9], 10], 11], 12]))
+
+def pow(x,n):
+    if n == 0:
+        return 1
+    else:
+        return x * pow(x,n-1)
+#
+# print(pow(2,3))
