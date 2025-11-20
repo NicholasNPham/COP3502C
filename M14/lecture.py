b9ce6b863478466b83c0ceeee4713fc891995032
NicholasNPham <phamn1000@gmail.com>
2025-11-19 21:31:43 -0500

    finished introduction

---
+++ M14/lecture.py
@@ -0,0 +1,13 @@
+"""
+
+Overview:
+- Read data from files
+- Write data to new/existing files
+- Handle data with different file formats
+    - Text files
+    - Comma separated values files (csv)
+    - Binary files
+- Understanding of interacting with file systems in your OS
+- How to plot and style aggregate data using matplotlib Python Module
+
+"""
