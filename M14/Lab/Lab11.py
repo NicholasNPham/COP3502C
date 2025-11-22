2b11c4ddcc542c8a9f960d357b2f9db45480ec9a
NicholasNPham <phamn1000@gmail.com>
2025-11-22 15:43:08 -0500

    finished lab 11

---
+++ M14/Lab/Lab11.py
@@ -0,0 +1,113 @@
+import os
+import matplotlib.pyplot as plt
+
+# file_path_student = r"data\students.txt"
+# file_path_assignment = r"data\assignments.txt"
+
+def main():
+    student_data_dict = {}
+
+    with open("data/students.txt", 'r') as file:
+        for line in file:
+            student_id = int(line[0:3])
+            name = line[3:].strip()
+            student_data_dict[name] = student_id
+
+    # print(student_data_dict)
+
+    assign_id_to_total_points = {}
+    assign_name_to_assign_id = {}
+
+    with open("data/assignments.txt", 'r') as file:
+        lines = file.readlines()
+        for i in range(0, len(lines), 3):
+            assign_id_to_total_points[int(lines[i + 1].strip())] = int(lines[i + 2].strip())
+            assign_name_to_assign_id[lines[i].strip()] = int(lines[i + 1].strip())
+
+    # print(assign_id_to_total_points)
+    # print(assign_name_to_assign_id)
+
+    submissions_dict = {}
+
+    files = os.listdir("data/submissions")
+    for filename in files:
+        with open(f"data/submissions/{filename}", 'r') as file:
+            split_info = file.readline().split("|")
+
+            student_id = int(split_info[0])
+            assignment_id = int(split_info[1])
+            grade = float(split_info[2])
+
+            if student_id not in submissions_dict:
+                submissions_dict[student_id] = {}
+
+            submissions_dict[student_id][assignment_id] = grade
+
+    # print(submissions_dict)
+
+    print("1. Student grade")
+    print("2. Assignment statistics")
+    print("3. Assignment graph\n")
+    selection = input("Enter your selection: ")
+
+    if selection == "1":
+
+        student_name = input("What is the student's name: ") # Michael Potter
+
+
+        if student_name not in student_data_dict:
+            print("Student name not found")
+        else:
+
+            total_points_overall = 0
+            total_points_earn = 0
+
+            student_id = student_data_dict[student_name] #174
+
+            for assignment_id, grade_percentage in submissions_dict[student_id].items():
+                total_points_earn += assign_id_to_total_points[assignment_id] * (submissions_dict[student_id][assignment_id] / 100 )
+                total_points_overall += assign_id_to_total_points[assignment_id]
+
+            average = (total_points_earn / total_points_overall) * 100
+
+            print(f"{round(average)}%")
+
+    elif selection == "2":
+
+        assignment_name = input("What is the assignment name: ")
+
+        if assignment_name not in assign_name_to_assign_id:
+            print("Assignment not found")
+        else:
+
+            grade = []
+
+            assignment_id = assign_name_to_assign_id[assignment_name]
+
+            for student in submissions_dict:
+                grade.append(submissions_dict[student][assignment_id])
+
+            print(f"Min: {int(min(grade))}%")
+            print(f"Avg: {int(sum(grade) / len(grade))}%")
+            print(f"Max: {int(max(grade))}%")
+
+
+    elif selection == "3":
+        assignment_name = input("What is the assignment name: ")
+
+        if assignment_name not in assign_name_to_assign_id:
+            print("Assignment not found")
+        else:
+
+            scores = []
+
+            assignment_id = assign_name_to_assign_id[assignment_name]
+
+            for student in submissions_dict:
+                scores.append(submissions_dict[student][assignment_id])
+
+            plt.hist(scores, bins=[45, 50, 55, 60, 65, 70, 75, 80, 85, 90, 95, 100])
+            plt.show()
+
+if __name__ == "__main__":
+    main()
--- M14/Lectures/lecture3.py
+++ M14/Lectures/lecture3.py
@@ -10,14 +10,14 @@ Walking Directories
 
 # OS Module Example
 import os
-my_file = open('E:/PycharmProjects/COP3502C/M14/COP3502/hello.txt', 'r')
-file_info = os.stat('E:/PycharmProjects/COP3502C/M14/COP3502/hello.txt')
+my_file = open('/M14/COP3502/hello.txt', 'r')
+file_info = os.stat('/M14/COP3502/hello.txt')
 print(file_info)
 my_file.close()
 # os.remove('E:/PycharmProjects/COP3502C/M14/COP3502/hello.txt') # This Deletes the file.
 
 import os
-path = os.path.join('E:/PycharmProjects/COP3502C/')
+path = os.path.join('//')
 for dir_name, sub_dirs, files, in os.walk(path):
     # print(dir_name, 'contains subdirectories:', sub_dirs, end="  ")
     # print("and the files:", files)
